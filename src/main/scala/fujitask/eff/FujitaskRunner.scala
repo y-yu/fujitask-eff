02121d2e482cc522db31a5291943826c85ee0e0d
Sanshiro Yoshida <halcat0x15a@gmail.com>
2019-06-09 10:13:04 +0900

    Simplify

--- src/main/scala/fujitask/eff/FujitaskRunner.scala
+++ src/main/scala/fujitask/eff/FujitaskRunner.scala
@@ -1,7 +1,7 @@
 package fujitask.eff
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future
 
 trait FujitaskRunner[I] {
-  def apply[A](task: I => ExecutionContext => Future[A]): Future[A]
+  def apply[A](task: I => Future[A]): Future[A]
 }
